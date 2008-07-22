858cbfff1c3a5432cbc3e9f35268b4b4e3ac656c
Matt Wright <matt@sysctl.co.uk>
2008-07-22 17:56:26 +0100

    Added new about box

---
+++ fink_env_vars.sh
@@ -0,0 +1,7 @@
+export CFLAGS=-I/sw/include 
+export LDFLAGS=-L/sw/lib 
+export CXXFLAGS=$CFLAGS 
+export CPPFLAGS=$CXXFLAGS 
+export ACLOCAL_FLAGS="-I /sw/share/aclocal"
+export PKG_CONFIG_PATH="/sw/lib/pkgconfig"
+export MACOSX_DEPLOYMENT_TARGET=10.4
