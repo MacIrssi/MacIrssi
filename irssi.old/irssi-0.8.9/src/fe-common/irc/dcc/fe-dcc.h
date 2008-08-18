07c31c21cb9c72c9934a7586e0e2578ff830e760
Matt Wright <matt@Caesium.local>
2008-08-18 02:45:23 +0100

    Removed irssi.old/

--- irssi.old/irssi-0.8.9/src/fe-common/irc/dcc/fe-dcc.h
+++
@@ -1,7 +0,0 @@
-#ifndef __FE_DCC_H
-#define __FE_DCC_H
-
-char *dcc_get_size_str(uoff_t size);
-void dcc_list_print_file(FILE_DCC_REC *dcc);
-
-#endif
--- irssi.old/irssi-0.8.9/src/fe-text/module.h
+++
@@ -1,8 +0,0 @@
-#include "common.h"
-#include "term.h"
-
-#define MODULE_NAME "fe-text"
-
-extern int quitting;
-void irssi_redraw(void);
-void irssi_set_dirty(void);
