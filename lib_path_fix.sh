1bd293b48655c8b49061631cb0c322c53c7e31f6
Matt Wright <matt@sysctl.co.uk>
2008-08-18 02:17:17 +0100

    Fixed lib_path_fix /Should/ have all required libraries installed and linked into Frameworks/ at compile time now Warning: If the library locations change, all hell breaks loose

--- lib_path_fix.sh
+++ lib_path_fix.sh
@@ -4,27 +4,27 @@
 echo "Fixing library name paths"
 
 #MacIrssi
-install_name_tool -change /opt/local/lib/libgmodule-2.0.0.dylib @executable_path/../Frameworks/libgmodule-2.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
-install_name_tool -change /usr/local/lib/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib-2.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
-install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
-install_name_tool -change /opt/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
+install_name_tool -change /usr/local/lib/libgmodule-2.0.0.dylib @executable_path/../Frameworks/libgmodule-2.0.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
+install_name_tool -change /usr/local/lib/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib-2.0.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
+install_name_tool -change /usr/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
+install_name_tool -change /usr/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.0.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/MacOS/MacIrssi
 
 #Theme preview daemon
-install_name_tool -change /opt/local/lib/libgmodule-2.0.0.dylib @executable_path/../Frameworks/libgmodule-2.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
-install_name_tool -change /usr/local/lib/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib-2.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
-install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
-install_name_tool -change /opt/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
+install_name_tool -change /usr/local/lib/libgmodule-2.0.0.dylib @executable_path/../Frameworks/libgmodule-2.0.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
+install_name_tool -change /usr/local/lib/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib-2.0.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
+install_name_tool -change /usr/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
+install_name_tool -change /usr/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.0.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/ThemePreviewDaemon/ThemePreviewDaemon
 
 #libglib
-install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libglib-2.0.dylib
-install_name_tool -change /opt/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libglib-2.0.dylib
+install_name_tool -change /usr/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libglib-2.0.0.dylib
+install_name_tool -change /usr/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.0.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libglib-2.0.0.dylib
 
 #libgmodule
-install_name_tool -change /opt/local/lib/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib-2.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libgmodule-2.0.dylib
-install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libgmodule-2.0.dylib
-install_name_tool -change /opt/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libgmodule-2.0.dylib
+install_name_tool -change /usr/local/lib/libglib-2.0.0.dylib @executable_path/../Frameworks/libglib-2.0.0.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libgmodule-2.0.0.dylib
+install_name_tool -change /usr/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libgmodule-2.0.0.dylib
+install_name_tool -change /usr/local/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.0.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libgmodule-2.0.0.dylib
 
 #libintl
-install_name_tool -change /opt/local/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libintl.dylib
+install_name_tool -change /usr/lib/libiconv.2.dylib @executable_path/../Frameworks/libiconv.2.dylib $TARGET_BUILD_DIR/MacIrssi.app/Contents/Frameworks/libintl.8.0.2.dylib
 
 exit 0
