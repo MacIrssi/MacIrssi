5870107eedf673fb9c48ce4a22d1df20f4ba729f
Matt Wright <matt@Caesium.local>
2008-12-27 23:42:08 +0000

    Fucking whitespaces and {} style!

--- Source/NSAttributedStringAdditions.m
+++ Source/NSAttributedStringAdditions.m
@@ -30,81 +30,94 @@ static NSURL* findURL(NSString* string);
 
 - (void)detectURLs:(NSColor*)linkColor
 {
-    NSScanner*					scanner;
-    NSRange						scanRange;
-    NSString*					scanString;
-    NSCharacterSet*				whitespaceSet;
-    NSURL*						foundURL;
-    NSDictionary*				linkAttr;
+  NSScanner*					scanner;
+  NSRange						scanRange;
+  NSString*					scanString;
+  NSCharacterSet*				whitespaceSet;
+  NSURL*						foundURL;
+  NSDictionary*				linkAttr;
+  
+  // Create our scanner and supporting delimiting character set
+  scanner = [NSScanner scannerWithString:[self string]];
+  whitespaceSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+  
+  // Start Scan
+  while( ![scanner isAtEnd] )
+  {
+    // Pull out a token delimited by whitespace or new line
+    [scanner scanUpToCharactersFromSet:whitespaceSet intoString:&scanString];
+    scanRange.length = [scanString length];
+    scanRange.location = [scanner scanLocation] - scanRange.length;
     
-    // Create our scanner and supporting delimiting character set
-    scanner = [NSScanner scannerWithString:[self string]];
-    whitespaceSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
-    
-    // Start Scan
-    while( ![scanner isAtEnd] )
+    // If we find a url modify the string attributes
+    if(( foundURL = findURL(scanString) ))
     {
-        // Pull out a token delimited by whitespace or new line
-        [scanner scanUpToCharactersFromSet:whitespaceSet intoString:&scanString];
-        scanRange.length = [scanString length];
-        scanRange.location = [scanner scanLocation] - scanRange.length;
-        
-        // If we find a url modify the string attributes
-        if(( foundURL = findURL(scanString) ))
-        {
-            // Apply underline style and link color
-            linkAttr = [NSDictionary dictionaryWithObjectsAndKeys:
-                            foundURL, NSLinkAttributeName,
-							[NSNumber numberWithInt:NSSingleUnderlineStyle], NSUnderlineStyleAttributeName,
-                            linkColor, NSForegroundColorAttributeName, 
-							NULL ];
-            [self addAttributes:linkAttr range:scanRange];
-        }
+      // Apply underline style and link color
+      linkAttr = [NSDictionary dictionaryWithObjectsAndKeys:
+                  foundURL, NSLinkAttributeName,
+                  [NSNumber numberWithInt:NSSingleUnderlineStyle], NSUnderlineStyleAttributeName,
+                  linkColor, NSForegroundColorAttributeName, 
+                  NULL ];
+      [self addAttributes:linkAttr range:scanRange];
     }
+  }
 }
 
 @end
 
- 
+
 NSURL* findURL(NSString* string)
 {
-    NSRange		theRange;
-    
-    // Look for ://
-    theRange = [string rangeOfString:@"://"];
-    if( theRange.location != NSNotFound && theRange.length != 0 )
-        return [NSURL URLWithString:string];
-    
-    // Look for www. at start
-    if( [string hasPrefix:@"www."] )
-        return [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", string]];
-    
-    // Look for ftp. at start
-    if( [string hasPrefix:@"ftp."] )
-        return [NSURL URLWithString:[NSString stringWithFormat:@"ftp://%@", string]];
-    
+  NSRange		theRange;
+  
+  // Look for ://
+  theRange = [string rangeOfString:@"://"];
+  
+  if( theRange.location != NSNotFound && theRange.length != 0 )
+  {
+    return [NSURL URLWithString:string];
+  }
+  
+  // Look for www. at start
+  if( [string hasPrefix:@"www."] )
+  {
+    return [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", string]];
+  }
+  
+  // Look for ftp. at start
+  if( [string hasPrefix:@"ftp."] )
+  {
+    return [NSURL URLWithString:[NSString stringWithFormat:@"ftp://%@", string]];
+  }
+  
 	// Look for mailto: at start
-    if( [string hasPrefix:@"mailto:"] )
-        return [NSURL URLWithString:string];
-	
+  if( [string hasPrefix:@"mailto:"] )
+  {
+    return [NSURL URLWithString:string];
+  }
+  
 #if 0
-    // Look for gopher. at start
-    if( [string hasPrefix:@"gopher."] )
-        return [NSURL URLWithString:[NSString stringWithFormat:@"gopher://%@", string]];
-    
-    // Look for nap: at start
-    if( [string hasPrefix:@"nap:"] )
-        return [NSURL URLWithString:string];
+  // Look for gopher. at start
+  if( [string hasPrefix:@"gopher."] )
+  {
+    return [NSURL URLWithString:[NSString stringWithFormat:@"gopher://%@", string]];
+  }
+  
+  // Look for nap: at start
+  if( [string hasPrefix:@"nap:"] )
+  {
+    return [NSURL URLWithString:string];
+  }
 #endif   
 	
 #if 0
 	/* DISABLED - IRC who-entries get f**ked up (Nils Hjelte) */
-    // Look for @ - minimum of a@a.a
-    theRange = [string rangeOfString:@"@"];
-    if( theRange.location != NSNotFound && theRange.length != 0
-        && theRange.location > 0 && theRange.location < [string length] - 3 
-        && [string rangeOfString:@"." options:0 range:NSMakeRange(theRange.location+1, [string length] - theRange.location-1)].location != NSNotFound)        return [NSURL URLWithString:[NSString stringWithFormat:@"mailto:%@",string]];
+  // Look for @ - minimum of a@a.a
+  theRange = [string rangeOfString:@"@"];
+  if( theRange.location != NSNotFound && theRange.length != 0
+     && theRange.location > 0 && theRange.location < [string length] - 3 
+     && [string rangeOfString:@"." options:0 range:NSMakeRange(theRange.location+1, [string length] - theRange.location-1)].location != NSNotFound)        return [NSURL URLWithString:[NSString stringWithFormat:@"mailto:%@",string]];
 #endif
 	
-    return nil;
+  return nil;
 }
