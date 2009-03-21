97831a8c6cc8da10ca9ff7087068186bf3f75476
Matt Wright <matt@sysctl.co.uk>
2009-03-21 01:53:39 +0000

    Working MILib builds. Removed some random cruft.

--- universal_env_vars.sh
+++
@@ -1,7 +0,0 @@
-export CFLAGS="-I/usr/local/include -arch i386 -arch ppc"
-export LDFLAGS="-L/usr/local/lib -arch i386 -arch ppc"
-#export CXXFLAGS=$CFLAGS 
-#export CPPFLAGS=$CXXFLAGS 
-export ACLOCAL_FLAGS="-I /usr/local/share/aclocal"
-export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
-export MACOSX_DEPLOYMENT_TARGET=10.4
