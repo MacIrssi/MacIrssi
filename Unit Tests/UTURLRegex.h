3faf0d94780bab2db89b3cf1e19c7cba1ad30f79
Matt Wright <matt@sysctl.co.uk>
2009-08-30 00:59:43 +0100

    Added an unused unit test

---
+++ Unit Tests/UTURLRegex.h
@@ -0,0 +1,16 @@
+//
+//  UTURLRegex.h
+//  MacIrssi
+//
+//  Created by Matt Wright on 29/08/2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface UTURLRegex : SenTestCase {
+
+}
+
+@end
---
+++ Unit Tests/UTURLRegex.m
@@ -0,0 +1,14 @@
+//
+//  UTURLRegex.m
+//  MacIrssi
+//
+//  Created by Matt Wright on 29/08/2009.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "UTURLRegex.h"
+
+
+@implementation UTURLRegex
+
+@end
