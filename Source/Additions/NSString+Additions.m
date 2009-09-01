e91a2b9c6fee16cc1dfb4344d795757ec146f95e
Matt Wright <matt@sysctl.co.uk>
2009-09-02 00:36:33 +0100

    Trim down URL recognition even more, only deal with things with a protocol specifier. Also, recognise urls such as http://caesium/

--- Source/Additions/NSString+Additions.m
+++ Source/Additions/NSString+Additions.m
@@ -33,19 +33,13 @@
   NSMutableArray *urls = [NSMutableArray array];
   
   // first thing is first, we're gonna have to build a URL regex, handily, here is one I made earlier
-  NSString *pattern =  @"(("
-                          "([a-zA-Z0-9]+://)" // protocol
-                          "(([0-9a-zA-Z_!~*'().&=+$%-]+: )?[0-9a-zA-Z_!~*'().&=+$%-]+@)?" // user@ 
-                          "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP- 199.194.52.184 
-                          "|" // allows either IP or domain 
-                          "([0-9a-zA-Z_!~'-]+\\.)*" // tertiary domain(s)- www. 
-                          "([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\\." // second level domain 
-                          "[a-zA-Z]{2,6})" // first level domain- .com or .museum 
-                        ")|("
-                          "([0-9a-zA-Z_!~'-]+\\.)+" // tertiary domain(s)- www. 
-                          "([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\\." // second level domain 
-                          "[a-zA-Z]{2,6}" // first level domain- .com or .museum 
-                        "))"
+  NSString *pattern =  @"([a-zA-Z0-9]+://)" // protocol
+                        "(([0-9a-zA-Z_!~*'().&=+$%-]+: )?[0-9a-zA-Z_!~*'().&=+$%-]+@)?" // user@ 
+                        "(([0-9]{1,3}\\.){3}[0-9]{1,3}" // IP- 199.194.52.184 
+                        "|" // allows either IP or domain 
+                        "([0-9a-zA-Z_!~'-]+\\.)*" // tertiary domain(s)- www. 
+                        "(([0-9a-zA-Z][0-9a-zA-Z-]{0,61})?[0-9a-zA-Z]\\.)?" // second level domain 
+                        "[a-zA-Z]+)" // first level domain- .com or .museum 
                         "(:[0-9]{1,4})?" // port number- :80 
                         "((/?)|" // a slash isn't required if there is no file name 
                         "((/[0-9a-zA-Z_!~*'.;?:@&=+$,%#-]+)|\\(([0-9a-zA-Z_!~*'.;?:@&=+$,%#-]+)\\))+/?)";
