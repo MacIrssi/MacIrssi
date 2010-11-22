560a2948414f36d091e8c996174193c10171c43d
Matt Wright <matt@sysctl.co.uk>
2010-11-21 18:52:18 -0800

    Add the git-rev-list count to the build number for beta builds.

--- xcodescripts/updateGitVersion.sh
+++ xcodescripts/updateGitVersion.sh
@@ -3,22 +3,30 @@
 # Updated for git by Marcus S. Zarra and Matt Long
  
 use strict;
- 
+
 # Get the current git commit hash and use it to set the CFBundleVersion value
 my $REV = `git show-ref --hash --abbrev --head HEAD | head -1`;
+my $INC = `git rev-list HEAD | wc -l`;
 my $INFO = "$ENV{BUILT_PRODUCTS_DIR}/$ENV{WRAPPER_NAME}/Contents/Info.plist";
+$INC =~ s/\s*(.*)\s*/$1/;
  
 my $version = $REV;
+my $increment = ".$INC";
 die "$0: No Git revision found" unless $version;
  
 chomp($version);
+chomp($increment);
 
 open(FH, "$INFO") or die "$0: $INFO: $!";
 my $info = join("", <FH>);
 close(FH);
  
 $info =~ s/([\t ]+<key>NSGitRevision<\/key>\n[\t ]+<string>).*?(<\/string>)/$1$version$2/;
+$info =~ s/([\t ]+<key>CFBundleVersion<\/key>\n[\t ]+<string>)(.*?)(<\/string>)/$1$2$increment$3/;
  
 open(FH, ">$INFO") or die "$0: $INFO: $!";
 print FH $info;
 close(FH);
+
+# Last thing, touch the original Info.plist so that Xcode always refreshes it.
+`touch $ENV{PROJECT_DIR}/$ENV{INFOPLIST_FILE}`
