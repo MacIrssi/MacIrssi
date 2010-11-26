628be05611676a931dc690f8878e0ca1b0f221a4
Matt Wright <matt@sysctl.co.uk>
2010-11-26 13:46:49 -0800

    Added MISearchBar.{h,m}

---
+++ Source/Custom Views/Search Bar/MISearchBar.m
@@ -0,0 +1,43 @@
+/*
+ MISearchBar.m
+ Copyright (c) 2010 Matt Wright.
+ 
+ MacIrssi is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#import "MISearchBar.h"
+
+
+@implementation MISearchBar
+
+- (id)initWithFrame:(NSRect)frame
+{
+  if (self = [super initWithFrame:frame])
+  {
+    
+  }
+  return self;
+}
+
+- (void)dealloc
+{
+  [super dealloc];
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+
+}
+
+@end
