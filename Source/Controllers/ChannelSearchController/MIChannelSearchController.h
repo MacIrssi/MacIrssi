81e4fb244dd285f210fd2d2946b1fa83d06c08c1
Matt Wright <matt@sysctl.co.uk>
2010-11-26 23:36:35 -0800

    Gorram license.

--- Source/Controllers/ChannelSearchController/MIChannelSearchController.h
+++ Source/Controllers/ChannelSearchController/MIChannelSearchController.h
@@ -1,10 +1,20 @@
-//
-//  MIChannelSearchController.h
-//  MacIrssi
-//
-//  Created by Matt Wright on 11/26/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
+/*
+ MIChannelSearchController.h
+ Copyright (c) 2010 Matt Wright.
+ 
+ MacIrssi is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #import <Cocoa/Cocoa.h>
 #import "MISearchBar.h"
