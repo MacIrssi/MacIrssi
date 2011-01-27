4b2ceb375191d346636649a035d7004d5f3148d5
Matt Wright <matt@sysctl.co.uk>
2011-01-26 20:52:50 -0800

    Tidying.

--- xcodescripts/generatePerlModule.sh
+++ xcodescripts/generatePerlModule.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 # generatePerlModule.sh [dir] [module name]
 
-[ ! -d $DERIVED_SOURCES_DIR ] && mkdir $DERIVED_SOURCES_DIR
+mkdir -p "$DERIVED_SOURCES_DIR"
 for x in $1/*.xs; do
-  OTHER_TYPEMAPS=""
-  if [ -f $1/../common/typemap ]; then
-    OTHER_TYPEMAPS="-typemap ../common/typemap"
-  fi
-  [ $x -nt "$DERIVED_SOURCES_DIR/`basename $x .xs`.c" ] && (xsubpp -typemap $1/typemap $OTHER_TYPEMAPS $x > $DERIVED_SOURCES_DIR/`basename $x .xs`.c || exit 1)
+  [ $x -nt "$DERIVED_SOURCES_DIR/`basename $x .xs`.c" ] && (xsubpp -typemap $1/typemap -typemap ../common/typemap $x > $DERIVED_SOURCES_DIR/`basename $x .xs`.c || exit 1)
 done
 
 CCOPTS=$DERIVED_SOURCES_DIR/compiler.opts
