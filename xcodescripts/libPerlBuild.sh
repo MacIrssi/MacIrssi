94a4eee22229da3ad1982b85b80de4e57e5a5862
Matt Wright <matt@sysctl.co.uk>
2011-02-11 00:35:29 -0800

    Use the script phase to build the perl binary modules.

--- xcodescripts/libPerlBuild.sh
+++ xcodescripts/libPerlBuild.sh
@@ -15,7 +15,7 @@ LDFLAGS="-isysroot $SDKROOT -Wl,-undefined,dynamic_lookup -fstack-protector -lpe
 
 RC_ARCHS=""
 for x in $ARCHS; do
-  RC_ARCHS="$RC_ARCHS -arch $x"
+	RC_ARCHS="$RC_ARCHS -arch $x"
 done
 
 DSTROOT="$TARGET_BUILD_DIR/$FULL_PRODUCT_NAME/Contents/Resources/Perl"
@@ -25,10 +25,14 @@ P="$SRCROOT/irssi/src/perl/"
 PERL_CORE="$P/perl-core.c $P/perl-common.c $P/perl-signals.c $P/perl-sources.c"
 FE_PERL="$P/module-formats.c $P/perl-fe.c"
 
+BINARY_MODULES="common Irc UI"
+
 # right, iterate the libs
 for lib in $PERLLIB/*; do
 	V=`basename $lib`
 	[[ $V == "5.8.1" ]] && continue
+
+	_CFLAGS="$CFLAGS -I$lib/darwin-thread-multi-2level/CORE"
 
 	if [ -e $lib/darwin-thread-multi-2level/CORE/libperl.dylib ]; then
 		CORE_SRCS=""
@@ -37,10 +41,9 @@ for lib in $PERLLIB/*; do
 		# someone is going to burn me in a firey hell for this
 		for x in $PERL_CORE; do
 			OBJ="$OBJECT_FILE_DIR-$CURRENT_VARIANT"/`basename $x .c`.$V.o
-      
+
 			if [[ $x -nt $OBJ ]]; then
-				_CFLAGS="$CFLAGS -I$lib/darwin-thread-multi-2level/CORE"
-        $CC -isysroot $SDKROOT $_CFLAGS $RC_ARCHS -o $OBJ -c $x || exit 1
+				$CC -isysroot $SDKROOT $_CFLAGS $RC_ARCHS -o $OBJ -c $x || exit 1
 			fi
 
 			CORE_SRCS="$CORE_SRCS $OBJ"
@@ -50,8 +53,7 @@ for lib in $PERLLIB/*; do
 			OBJ="$OBJECT_FILE_DIR-$CURRENT_VARIANT"/`basename $x .c`.$V.o
 
 			if [[ $x -nt $OBJ ]]; then
-				_CFLAGS="$CFLAGS -I$lib/darwin-thread-multi-2level/CORE"
-        $CC -isysroot $SDKROOT $_CFLAGS $RC_ARCHS -o $OBJ -c $x || exit 1
+				$CC -isysroot $SDKROOT $_CFLAGS $RC_ARCHS -o $OBJ -c $x || exit 1
 			fi
 
 			FE_SRCS="$FE_SRCS $OBJ"
@@ -59,5 +61,35 @@ for lib in $PERLLIB/*; do
 
 		$CC -dynamiclib $LDFLAGS $RC_ARCHS -L$lib/darwin-thread-multi-2level/CORE -o $DSTROOT/libperl_core.$V.dylib $CORE_SRCS || exit 1
 		$CC -dynamiclib $LDFLAGS $RC_ARCHS -L$lib/darwin-thread-multi-2level/CORE -o $DSTROOT/libfe_perl.$V.dylib $FE_SRCS || exit 1
+
+		# now build the perl modules
+		for module in $BINARY_MODULES; do
+			mkdir -p "$DERIVED_SOURCES_DIR/perl/$V/$module"
+
+			XS_INCLUDES="-isystem $lib/darwin-thread-multi-2level/CORE -I$SRCROOT/irssi/src/perl/$module -I$SRCROOT/irssi/src/fe-common/core -I$SRCROOT/irssi/src/core -I$SRCROOT/irssi/src/irc -I$SRCROOT/irssi/src/irc/core -I$SRCROOT/irssi/src/core -I$SRCROOT/Headers"
+			XS_CFLAGS="-UHAVE_CONFIG_H -DVERSION=\"0.9\" -DXS_VERSION=\"0.9\""
+
+			for xs in $P/$module/*.xs; do
+				CFILE="$DERIVED_SOURCES_DIR/perl/$V/$module/`basename $xs .xs`.c"
+				[ $xs -nt $CFILE ] && (cd $P/$module ; echo xsubpp $V/$module/`basename $xs` ; xsubpp -typemap typemap -typemap ../common/typemap $xs > $CFILE || exit 2)
+				[ $? -eq 2 ] && exit 1
+			done
+
+			OBJECTS=""
+			for xc in $DERIVED_SOURCES_DIR/perl/$V/$module/*.c; do
+				OBJ="$DERIVED_SOURCES_DIR/perl/$V/$module/`basename $xc .c`.o"
+				[ $xc -nt $OBJ ] && (echo cc $V/$module/`basename $xc` ; $CC -isysroot $SDKROOT $XS_INCLUDES $_CFLAGS $XS_CFLAGS $RC_ARCHS -o $OBJ -c $xc || exit 2)
+				[ $? -eq 2 ] && exit 1
+				OBJECTS="$OBJECTS $OBJ"
+			done
+
+			if [ "x$module" == "xcommon" ]; then
+				mkdir -p "$DSTROOT/$V/auto/Irssi"
+				$CC -dynamiclib $RC_ARCHS -undefined dynamic_lookup -o "$DSTROOT/$V/auto/Irssi/Irssi.bundle" $OBJECTS || exit 1
+			else
+				mkdir -p "$DSTROOT/$V/auto/Irssi/$module"
+				$CC -dynamiclib $RC_ARCHS -undefined dynamic_lookup -o "$DSTROOT/$V/auto/Irssi/$module/$module.bundle" $OBJECTS || exit 1
+			fi
+		done
 	fi
 done
--- xcodescripts/libPerlCopy.sh
+++ xcodescripts/libPerlCopy.sh
@@ -9,30 +9,12 @@ DST="$BUILT_PRODUCTS_DIR/$FULL_PRODUCT_NAME/Contents/Resources/Scripts"
 # Irssi.pm
 cp $PERLPATH/common/Irssi.pm $DST/Irssi.pm
 
-# Irssi.bundle
-[ -f "$BUILT_PRODUCTS_DIR/Irssi.bundle" ] || exit 1
-mkdir -p "$DST/auto/Irssi" || exit 1
-cp "$BUILT_PRODUCTS_DIR/Irssi.bundle" "$DST/auto/Irssi/Irssi.bundle"
-touch "$DST/auto/Irssi/Irssi.bs"
-
 # UI.pm
 [ ! -d $DST/Irssi ] && mkdir -p $DST/Irssi
 cp $PERLPATH/ui/UI.pm $DST/Irssi/UI.pm
 
-# UI.bundle
-[ -f "$BUILT_PRODUCTS_DIR/UI.bundle" ] || exit 1
-mkdir -p "$DST/auto/Irssi/UI" || exit 1
-cp "$BUILT_PRODUCTS_DIR/UI.bundle" "$DST/auto/Irssi/UI/UI.bundle"
-touch "$DST/auto/Irssi/UI/UI.bs"
-
 # Irc.pm
 [ ! -d $DST/Irssi ] && mkdir -p $DST/Irssi
 cp $PERLPATH/irc/Irc.pm $DST/Irssi/Irc.pm
-
-# Irc.bundle
-[ -f "$BUILT_PRODUCTS_DIR/Irc.bundle" ] || exit 1
-mkdir -p "$DST/auto/Irssi/Irc" || exit 1
-cp "$BUILT_PRODUCTS_DIR/Irc.bundle" "$DST/auto/Irssi/Irc/Irc.bundle"
-touch "$DST/auto/Irssi/Irc/Irc.bs"
 
 exit 0
